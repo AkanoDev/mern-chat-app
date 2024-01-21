ccd9b2bc1bf39b2fcf8d5d7b5aaa4099ef079131
AkanoDev <taalredickjake@gmail.com>
2024-01-21 19:45:15 +0800

    working with model

---
+++ server/Models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, minLength: 3, maxLenght: 30 },
+  email: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLenght: 300,
+    unique: true,
+  },
+  password: { type: String, required: true, minLength: 3, maxLenght: 1000 },
+
+  timestamps: true,
+});
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
