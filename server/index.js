fe66b7ae7384b831833b493d3d778246cb4076a9
AkanoDev <taalredickjake@gmail.com>
2024-01-21 19:31:52 +0800

    database config connection

--- server/index.js
+++ server/index.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT;
+app.get("/", (req, res) => {
+  res.json("hello here");
+});
+
+const port = process.env.PORT || 5000;
+const DATABASE = process.env.DATABASE_URI;
 
 app.listen(port, (req, res) => {
   console.log(`Running on port: ${port}`);
 });
 
-//RKhYxUNAH4Ub2WGC
-//mongodb+srv://taalredickjake:<password>@cluster0.obxhdq5.mongodb.net/?retryWrites=true&w=majority
+mongoose
+  .connect(DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DATABASE SUCCESSFULLY CONNECTED"))
+  .catch((err) => {
+    console.log("DATABASE CONNECTION FAILED", err.message);
+  });
+
+module.exports = app;
